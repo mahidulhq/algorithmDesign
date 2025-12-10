0103a8ae3c0a331a3c506d1c8abd3d25e105e5fc
Mahidul Haque <161691748+mahidulhq@users.noreply.github.com>
2025-12-10 07:19:54 +0600

    fix

--- mine/knapsackZeroOne.cpp
+++ mine/knapsackZeroOne.cpp
@@ -5,10 +5,11 @@ int w[1005],v[1005];
 int dp[1005][1005];
 int knapsack(int i, int c){
     if(i < 0 || c == 0) return 0;
+    if(dp[i][c] != -1) return dp[i][c];
     if(c >= w[i]){
-        int op1 = knapsack(i-1, c - w[i]) + v[i];
-        int op2 = knapsack(i-1, c);
-        return dp[i][c] = max(op1,op2);
+        int op1 = knapsack(i-1 , c - w[i]) + v[i];
+        int op2 = knapsack(i-1 ,c);
+        return dp[i][c] = max(op1, op2);
     } else return dp[i][c] = knapsack(i-1,c);
 }
 
@@ -16,9 +17,9 @@ int main(){
     memset(dp,-1,sizeof(dp));
     int c; cin >> c;
     int n; cin >> n;
-    for(int i = 0;i < n;i++) cin >> w[i];
-    for(int i = 0;i < n;i++) cin >> v[i];
-
+    for(int i = 0; i<n;i++) cin >> w[i];
+    for(int i = 0; i<n;i++) cin >> v[i];
+    
     cout << knapsack(n-1,c);
     return 0;
 }
